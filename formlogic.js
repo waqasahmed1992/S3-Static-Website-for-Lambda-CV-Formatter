75d6b65f0f55f56f28331a20f82533c15c2516ce
waqasahmed1992 <46716439+waqasahmed1992@users.noreply.github.com>
2022-06-19 20:28:52 +0500

    Add files via upload

---
+++ formlogic.js
@@ -0,0 +1,45 @@
+var API_ENDPOINT = 'https://g2vff8vt9d.execute-api.us-east-1.amazonaws.com/prod/reminders';
+
+var errorDiv = document.getElementById('error-message')
+var successDiv = document.getElementById('success-message')
+var resultsDiv = document.getElementById('results-message')
+
+function messageValue() { return document.getElementById('message').value }
+function emailValue() { return document.getElementById('email').value }
+
+
+function clearNotifications() {
+    errorDiv.textContent = '';
+    resultsDiv.textContent = '';
+    successDiv.textContent = '';
+}
+
+document.getElementById('emailButton').addEventListener('click', function(e) {
+    sendData(e, 'email');
+});
+
+function sendData (e) {
+    e.preventDefault()
+    clearNotifications()
+    fetch(API_ENDPOINT, {
+        headers:{
+            "Content-type": "application/json"
+        },
+        method: 'POST',
+        body: JSON.stringify({
+            message: messageValue(),
+            email: emailValue(),
+        }),
+        mode: 'cors'
+    })
+    .then((resp) => resp.json())
+    .then(function(data) {
+        console.log(data)
+        successDiv.textContent = 'Looks ok. But check the result below!';
+        resultsDiv.textContent = JSON.stringify(data);
+    })
+    .catch(function(err) {
+        errorDiv.textContent = 'Yikes! There was an error:\n' + err.toString();
+        console.log(err)
+    });
+};
